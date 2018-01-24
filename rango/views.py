fa701d4823f10be5c6aa94ec2311a7895551a344
Jack Warnock <2243366ww@student.gla.ac.uk>
2018-01-24 15:17:57 +0000

    Chapter 5

--- rango/views.py
+++ rango/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'boldmessage': "Lol"}
+    return render(request, 'rango/about.html', context=context_dict)
